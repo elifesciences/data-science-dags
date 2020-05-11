eb2c22b37866f8b821a6eb51f4349ffe101303ea
michael <m.owonibi@elifesciences.org>
2020-05-11 12:24:56 +0100

    feat(data_science_dag): Install requirements
    Install required packages in the data pipeline image

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+pip install  --user -r $DIR/requirements.dag.txt
+pip install  --user -r $DIR/requirements.notebook.txt
 pip install -e $DIR/ --user
 cp $DIR/dags $1 -r
 cp $DIR/notebooks $2 -r
