a9d45ff6a15f45197982b6360c774d28792614c2
Daniel Ecer <de-code@users.noreply.github.com>
2020-05-18 16:23:10 +0100

    using dag as context manager

--- dags/peerscout_build_senior_editor_profiles.py
+++ dags/peerscout_build_senior_editor_profiles.py
@@ -14,12 +14,8 @@ LOGGER = logging.getLogger(__name__)
 DAG_ID = "PeerScout_Build_Senior_Editor_Profiles"
 
 
-DATA_SCIENCE_DAG = create_dag(
-    dag_id=DAG_ID
-)
-
-NOTEBOOK_RUN_TASK = create_run_notebook_operator(
-    notebook_filename='peerscout/peerscout-build-senior-editor-profiles.ipynb',
-    task_id='Run_Jupyter_Notebook2',
-    dag=DATA_SCIENCE_DAG
-)
+# Note: need to save dag to a variable
+with create_dag(dag_id=DAG_ID) as dag:
+    create_run_notebook_operator(
+        notebook_filename='peerscout/peerscout-build-senior-editor-profiles.ipynb'
+    )
--- dags/sample_data_science_pipeline.py
+++ dags/sample_data_science_pipeline.py
@@ -14,12 +14,8 @@ LOGGER = logging.getLogger(__name__)
 DAG_ID = "Sample_Data_Science_Data_Pipeline"
 
 
-DATA_SCIENCE_DAG = create_dag(
-    dag_id=DAG_ID
-)
-print('DATA_SCIENCE_DAG:', DATA_SCIENCE_DAG)
-
-NOTEBOOK_RUN_TASK = create_run_notebook_operator(
-    notebook_filename='example.ipynb',
-    dag=DATA_SCIENCE_DAG,
-)
+# Note: need to save dag to a variable
+with create_dag(dag_id=DAG_ID) as dag:
+    create_run_notebook_operator(
+        notebook_filename='example.ipynb',
+    )
