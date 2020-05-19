c75035a3fb525429d4d5492313ccb1f0d829a76f
Daniel Ecer <de-code@users.noreply.github.com>
2020-05-19 19:43:06 +0100

    added column also containing state

--- data_science_pipeline/sql/Manuscript_Version_Matching_Editor_Profile.sql
+++ data_science_pipeline/sql/Manuscript_Version_Matching_Editor_Profile.sql
@@ -12,42 +12,63 @@ WITH t_initial_submission_version_id_including_null AS (
     TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), manuscript_version.stages[SAFE_OFFSET(0)].stage_timestamp, DAY) <= 30
     OR NOT EXISTS (SELECT 1 FROM UNNEST(manuscript_version.stages) WHERE stage_name = 'Senior Editor Assigned')
   )
+),
+
+t_recommendations AS (
+  SELECT
+    Editor.*,
+    manuscript_version.manuscript_id AS Manuscript_ID,
+    manuscript_version.version_id AS Version_ID,
+    manuscript_version.long_manuscript_identifier AS Long_Manuscript_Identifier,
+    COALESCE(
+      CONCAT(manuscript_version.long_manuscript_identifier, ' - ', manuscript_version.manuscript_title),
+      '~ No Manuscript Selected ~'
+    ) AS Display_Manuscript_Identifier,
+    manuscript_version.overall_stage AS Overall_Stage,
+    manuscript_version.manuscript_title AS Manuscript_Title,
+    manuscript_version.abstract AS Abstract,
+    ARRAY_TO_STRING(
+      ARRAY(
+        SELECT keyword
+        FROM UNNEST(matching_keywords)
+      ),
+      ', '
+    ) AS Matching_Keywords_CSV,
+    editor_recommendation.score AS Matching_Score,
+    manuscript_version.stages[SAFE_OFFSET(0)].stage_name AS Last_Stage_Name,
+    (
+      SELECT stage_timestamp
+      FROM UNNEST(stages)
+      WHERE stage_name = 'Senior Editor Assigned'
+      ORDER BY stage_timestamp DESC
+      LIMIT 1
+    ) AS Senior_Editor_Assigned_Timestamp
+  FROM `{project}.{dataset}.mv_Editorial_Editor_Profile` AS Editor
+  CROSS JOIN t_initial_submission_version_id_including_null AS version_ids
+  LEFT JOIN `{project}.{dataset}.v_manuscript_version_last_editor_assigned_timestamp` AS manuscript_version
+    ON manuscript_version.version_id = version_ids.version_id
+  LEFT JOIN `{project}.{dataset}.data_science_editor_recommendation` AS editor_recommendation
+    ON editor_recommendation.person_id = Editor.Person_ID
+    AND editor_recommendation.version_id = version_ids.version_id
+  WHERE Editor.Role_Name = 'Senior Editor'
 )
 
 SELECT
-  Editor.*,
-  manuscript_version.manuscript_id AS Manuscript_ID,
-  manuscript_version.version_id AS Version_ID,
-  manuscript_version.long_manuscript_identifier AS Long_Manuscript_Identifier,
-  COALESCE(
-    CONCAT(manuscript_version.long_manuscript_identifier, ' - ', manuscript_version.manuscript_title),
-    '~ No Manuscript Selected ~'
-  ) AS Display_Manuscript_Identifier,
-  manuscript_version.overall_stage AS Overall_Stage,
-  manuscript_version.manuscript_title AS Manuscript_Title,
-  manuscript_version.abstract AS Abstract,
-  ARRAY_TO_STRING(
-    ARRAY(
-      SELECT keyword
-      FROM UNNEST(matching_keywords)
-    ),
-    ', '
-  ) AS Matching_Keywords_CSV,
-  editor_recommendation.score AS Matching_Score,
-  manuscript_version.stages[SAFE_OFFSET(0)].stage_name AS Last_Stage_Name,
-  (
-    SELECT stage_timestamp
-    FROM UNNEST(stages)
-    WHERE stage_name = 'Senior Editor Assigned'
-    ORDER BY stage_timestamp DESC
-    LIMIT 1
-  ) AS Senior_Editor_Assigned_Timestamp
-FROM `{project}.{dataset}.mv_Editorial_Editor_Profile` AS Editor
-CROSS JOIN t_initial_submission_version_id_including_null AS version_ids
-LEFT JOIN `{project}.{dataset}.v_manuscript_version_last_editor_assigned_timestamp` AS manuscript_version
-  ON manuscript_version.version_id = version_ids.version_id
-LEFT JOIN `{project}.{dataset}.data_science_editor_recommendation` AS editor_recommendation
-  ON editor_recommendation.person_id = Editor.Person_ID
-  AND editor_recommendation.version_id = version_ids.version_id
-WHERE Editor.Role_Name = 'Senior Editor'
-ORDER BY version_id DESC, editor_recommendation.score DESC
+  *,
+  IF(
+    Version_ID IS NULL,
+    Display_Manuscript_Identifier,
+    CONCAT(
+      Long_Manuscript_Identifier,
+      IF(Senior_Editor_Assigned_Timestamp IS NOT NULL, ' (SE assigned)', ''),
+      IF(
+        MAX(Matching_Score) OVER(PARTITION BY Version_ID) IS NULL,
+        ' (no recommendation yet)',
+        ''
+      ),
+      ' - ',
+      Manuscript_Title
+    )
+  ) AS Display_Manuscript_Identifier_With_State
+FROM t_recommendations
+ORDER BY Version_ID DESC, Matching_Score DESC
