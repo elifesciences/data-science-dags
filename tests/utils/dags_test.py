a0bb901538f4bb84132ab2562ddd0d739ab24e6d
Daniel Ecer <de-code@users.noreply.github.com>
2020-05-26 13:39:21 +0200

    fix state path to environment specific (#27)
    * changed state path to data-science
    
    * pass in state_path to notebooks

--- tests/utils/dags_test.py
+++ tests/utils/dags_test.py
@@ -1,6 +1,26 @@
+import os
+from unittest.mock import patch
+
 from data_science_pipeline.utils.dags import (
+    get_default_state_path,
     get_default_notebook_task_id
 )
+
+
+class TestGetDefaultStatePath:
+    @patch.dict(os.environ, {})
+    def test_should_default_to_state_path_using_deployment_env(self):
+        assert (
+            get_default_state_path('test')
+            == 's3://test-elife-data-pipeline/airflow-config/data-science/state'
+        )
+
+    @patch.dict(os.environ, {'DATA_SCIENCE_STATE_PATH': '/path/to/state'})
+    def test_should_use_configured_state_path(self):
+        assert (
+            get_default_state_path('test')
+            == '/path/to/state'
+        )
 
 
 class TestGetDefaultNotebookTaskId:
