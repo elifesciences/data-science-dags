e48e4c46e45eab80e1fa9084d30aa4a0ead6bb27
Daniel Ecer <de-code@users.noreply.github.com>
2020-06-17 19:21:47 +0100

    enable debug logging for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
+import logging
 from pathlib import Path
 
 import pytest
 from py._path.local import LocalPath
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_logging():
+    for name in {'tests', 'data_science_pipeline'}:
+        logging.getLogger(name).setLevel('DEBUG')
 
 
 @pytest.fixture
