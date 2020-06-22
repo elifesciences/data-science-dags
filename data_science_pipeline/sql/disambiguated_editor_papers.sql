e04bc5f740938990e7e810d1769d32f96f7d53d0
Daniel Ecer <de-code@users.noreply.github.com>
2020-06-22 15:25:38 +0100

    also include relevant_pubmed_ids

--- data_science_pipeline/sql/disambiguated_editor_papers.sql
+++ data_science_pipeline/sql/disambiguated_editor_papers.sql
@@ -1,6 +1,7 @@
 WITH t_editor_with_orcids AS (
   SELECT
-    Profile.Person_ID AS person_id,
+    editor.person_id,
+    editor.relevant_pubmed_ids,
     (
       SELECT reference_value
       FROM UNNEST(person.external_references)
@@ -17,9 +18,9 @@ WITH t_editor_with_orcids AS (
       ORDER BY start_timestamp, modified_timestamp
       LIMIT 1
     ) AS unverified_orcid
-  FROM `{project}.{dataset}.mv_Editorial_Editor_Profile` AS Profile
+  FROM `{project}.{dataset}.data_science_editor_pubmed_links` AS editor
   JOIN `{project}.{dataset}.mv_person_v2` AS person
-    ON person.person_id = Profile.person_id
+    ON person.person_id = editor.person_id
 ),
 
 t_editor_with_single_orcid AS (
@@ -37,13 +38,25 @@ t_pubmed_id_by_orcid AS (
   JOIN UNNEST(manuscript_summary.authorList.author) AS author
   WHERE author.authorId.type = 'ORCID'
     AND pmid IS NOT NULL
+),
+
+t_pubmed_id_by_person_id AS (
+  SELECT DISTINCT editor.person_id, pmid
+  FROM t_editor_with_single_orcid AS editor
+  JOIN t_pubmed_id_by_orcid AS paper
+    ON paper.orcid = editor.orcid
+
+  UNION DISTINCT
+
+  SELECT DISTINCT editor.person_id, pmid
+  FROM t_editor_with_single_orcid AS editor
+  JOIN UNNEST(relevant_pubmed_ids) AS pmid
 )
 
 SELECT
-  person_id,
-  ARRAY(
-    SELECT DISTINCT pmid
-    FROM t_pubmed_id_by_orcid AS paper
-    WHERE paper.orcid = editor.orcid
-  ) AS disambiguated_pubmed_ids
+  editor.person_id,
+  ARRAY_AGG(pmid IGNORE NULLS) AS disambiguated_pubmed_ids
 FROM t_editor_with_single_orcid AS editor
+LEFT JOIN t_pubmed_id_by_person_id AS paper
+  ON paper.person_id = editor.person_id
+GROUP BY editor.person_id
