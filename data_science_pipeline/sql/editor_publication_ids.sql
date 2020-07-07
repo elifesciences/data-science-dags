f705e3b6904e4f02a5e61d181fcb28e98f71953c
Daniel Ecer <de-code@users.noreply.github.com>
2020-07-07 11:56:38 +0200

    use pubmed manuscripts for BRE recommendation (#88)

---
+++ data_science_pipeline/sql/editor_publication_ids.sql
@@ -0,0 +1,34 @@
+WITH t_editor_pubmed_ids AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY person_id ORDER BY provenance.imported_timestamp DESC) AS person_id_row_number
+  FROM `{project}.{dataset}.data_science_editor_pubmed_ids`
+),
+
+t_paper_summary AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY pmid ORDER BY provenance.imported_timestamp DESC) AS pmid_row_number
+  FROM `{project}.{dataset}.data_science_external_manuscript_summary`
+)
+
+SELECT
+  disambiguated_editor_papers.person_id,
+  CONCAT('pmid:', pmid) AS publication_id,
+  pmid,
+  doi,
+  REGEXP_EXTRACT(LOWER(paper_summary.doi), r'10.7554/elife.([\d]{{5}})') AS manuscript_id,
+  paper_summary.firstPublicationDate AS publication_date,
+  (pmid IN UNNEST(editor_pubmed_links.relevant_pubmed_ids)) AS is_relevant_pubmed_id,
+  (pmid IN UNNEST(editor_pubmed_ids.pubmed_ids)) AS is_search_pubmed_id
+
+FROM `{project}.{dataset}.data_science_disambiguated_editor_papers` AS disambiguated_editor_papers
+JOIN `{project}.{dataset}.data_science_editor_pubmed_links` AS editor_pubmed_links
+  ON editor_pubmed_links.person_id = disambiguated_editor_papers.person_id
+JOIN t_editor_pubmed_ids AS editor_pubmed_ids
+  ON editor_pubmed_ids.person_id = disambiguated_editor_papers.person_id
+  AND editor_pubmed_ids.person_id_row_number = 1 
+JOIN UNNEST(disambiguated_editor_papers.disambiguated_pubmed_ids) AS pmid
+JOIN t_paper_summary AS paper_summary 
+  ON paper_summary.pmid = pmid
+  AND paper_summary.pmid_row_number = 1
---
+++ data_science_pipeline/sql/publications_extracted_keywords.sql
@@ -0,0 +1,27 @@
+WITH t_paper_extracted_keywords AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY pmid ORDER BY data_hub_imported_timestamp DESC) AS pmid_row_number
+  FROM `{project}.{dataset}.data_science_disambiguated_editor_papers_abstract_keywords`
+),
+
+t_paper_summary AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY pmid ORDER BY provenance.imported_timestamp DESC) AS pmid_row_number
+  FROM `{project}.{dataset}.data_science_external_manuscript_summary`
+)
+
+SELECT
+  CONCAT('pmid:', paper_extracted_keywords.pmid) AS publication_id,
+  paper_extracted_keywords.pmid,
+  paper_summary.doi,
+  REGEXP_EXTRACT(LOWER(paper_summary.doi), r'10.7554/elife.([\d]{{5}})') AS manuscript_id,
+  paper_summary.firstPublicationDate AS publication_date,
+  paper_extracted_keywords.extracted_keywords AS abstract_keywords
+FROM t_paper_extracted_keywords AS paper_extracted_keywords
+JOIN t_paper_summary AS paper_summary
+  ON paper_summary.pmid = paper_extracted_keywords.pmid
+  AND paper_summary.pmid_row_number = 1
+WHERE paper_extracted_keywords.pmid_row_number = 1
+  AND ARRAY_LENGTH(paper_extracted_keywords.extracted_keywords) > 0
