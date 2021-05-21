8f019374429b15051f4f075b9898d77afe7a1694
Hazal <HazalCiplak@users.noreply.github.com>
2021-05-21 09:36:32 +0100

    updated airflow and python version (#399)
    * updated airflow and python version
    
    * added pystan to the requirements
    
    * added pystan 3.1.1
    
    * removed pystan from requirements.notebook
    
    * added pystan to the requirements.dag
    
    * removed pytsan from requirements.dag file
    
    * added pystan version 2.19.1.1 to a new requirements file
    
    * added requrements file to docker files and install.sh
    
    * removed the line
    
    * added requirements requirements.fbprophet file
    
    * removed requirements from requirements.notebook which are already in fbprophet reqs
    
    * changed the structure as install all requirements before fbprophet

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-pip install  --user -r $DIR/requirements.dag.txt
-pip install  --user -r $DIR/requirements.notebook.txt
+pip install --user -r $DIR/requirements.dag.txt
+pip install --user -r $DIR/requirements.notebook.txt
+pip install --user -r $DIR/requirements.fbprophet.txt
 pip install -e $DIR/ --user
 cp $DIR/dags $1 -r
 cp $DIR/notebooks $2/ -r
