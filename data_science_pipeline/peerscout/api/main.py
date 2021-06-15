f4b265c57b9902679cc36413108a387c13cbfff0
HazalCiplak <hazalciplak@gmail.com>
2021-06-15 16:10:11 +0100

    configured main.py according to new inputs

--- data_science_pipeline/peerscout/api/main.py
+++ data_science_pipeline/peerscout/api/main.py
@@ -5,28 +5,18 @@ import os
 from abc import ABC, abstractmethod
 from typing import Optional
 from werkzeug.exceptions import BadRequest, Unauthorized
-
+import jsonschema
+import json
 
 LOGGER = logging.getLogger(__name__)
 
-output1 = {
+output = {
     "reviewing_editor_recommendation": {
-        "person_ids": [1, 2, 3],
+        "person_ids": [],
         "recommendation_html": "The recommended editors are ..."
     },
     "senior_editor_recommendation": {
-        "person_ids": [1, 2, 3],
-        "recommendation_html": "The recommended editors are ..."
-    }
-}
-
-output2 = {
-    "reviewing_editor_recommendation2": {
-        "person_ids": [1, 2, 3],
-        "recommendation_html": "The recommended editors are ..."
-    },
-    "senior_editor_recommendation2": {
-        "person_ids": [1, 2, 3],
+        "person_ids": [],
         "recommendation_html": "The recommended editors are ..."
     }
 }
@@ -86,22 +76,40 @@ def create_app():
 
     app = Flask(__name__)
 
+    @app.route('/', methods=['GET'])
+    def _home():
+        html = """<h1>PeerScout Recommendation API</h1>
+        <p>This site is a prototype PeerScout Recommendation API for
+        senior and reviewing editor recommendations.</p>"""
+        return html
+
     @app.route('/api/status', methods=['GET'])
     def _status():
-        return jsonify({'message': 'Welcome to PeerScout'})
+        return jsonify({"status": "OK"})
 
     @app.route('/api/peerscout', methods=['POST'])
     @route_wrapper
     def _peerscout_api():
         data = request.get_json(force=True)
+        with open('./data_science_pipeline/peerscout/api/input-json-schema.json') as f:
+            json_schema = json.load(f)
+
+        try:
+            jsonschema.validate(data, json_schema)
+        except jsonschema.exceptions.ValidationError as e:
+            print("invalid JSON:", e)
+
         try:
             abstract = data['abstract']
+            reviewing_editors = data['include_reviewing_editors_id']
+            senior_editors = data['include_senior_editors_id']
         except KeyError as e:
             raise BadRequest() from e
-        if abstract == 'output1':
-            return jsonify(output1)
-        elif abstract == 'output2':
-            return jsonify(output2)
+
+        if abstract != "":
+            output['reviewing_editor_recommendation']['person_ids'] = reviewing_editors
+            output['senior_editor_recommendation']['person_ids'] = senior_editors
+            return jsonify(output)
         else:
             raise BadRequest('no valid abstract provided')
     return app
