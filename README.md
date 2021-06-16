113c1422a540f1527d4d60676e9326ee1a97e151
Hazal <HazalCiplak@users.noreply.github.com>
2021-06-16 10:04:16 +0100

    Improve dummy api (#447)
    * edited readme
    
    * added jsonschema to requirements
    
    * added example input json files and valid json schema
    
    * configured main.py according to new inputs
    
    * corrected input json
    
    * added function to return json
    
    * fixed pylint errors
    
    * corrected type error
    
    * changed input json file keys
    
    * changed the sutructure in main.py
    
    * changed the structure of input json file
    
    * added jsonify to response

--- README.md
+++ README.md
@@ -9,11 +9,17 @@ PEERSCOUT_API_ACCESS_TOKEN='123' make peerscout-api-start
 ```
 
 
+To test the api
+
+```bash
+make peerscout-api-start
+```
+
 ```bash
 curl \
     --header 'X-Access-Token: 123' \
     --request POST \
-    --data '{"abstract":"output1"}' \
+    --data '@example-data/peerscout-api-request1.json' \
     http://localhost:8080/api/peerscout
 ```
 
@@ -23,6 +29,6 @@ or
 curl \
     --header 'X-Access-Token: 123' \
     --request POST \
-    --data '{"abstract":"output2"}' \
+    --data '@example-data/peerscout-api-request2.json' \
     http://localhost:8080/api/peerscout
 ```
--- data_science_pipeline/peerscout/api/main.py
+++ data_science_pipeline/peerscout/api/main.py
@@ -5,31 +5,12 @@ import os
 from abc import ABC, abstractmethod
 from typing import Optional
 from werkzeug.exceptions import BadRequest, Unauthorized
-
+import jsonschema
+import json
 
 LOGGER = logging.getLogger(__name__)
 
-output1 = {
-    "reviewing_editor_recommendation": {
-        "person_ids": [1, 2, 3],
-        "recommendation_html": "The recommended editors are ..."
-    },
-    "senior_editor_recommendation": {
-        "person_ids": [1, 2, 3],
-        "recommendation_html": "The recommended editors are ..."
-    }
-}
-
-output2 = {
-    "reviewing_editor_recommendation2": {
-        "person_ids": [1, 2, 3],
-        "recommendation_html": "The recommended editors are ..."
-    },
-    "senior_editor_recommendation2": {
-        "person_ids": [1, 2, 3],
-        "recommendation_html": "The recommended editors are ..."
-    }
-}
+REQUEST_JSON_SCHEMA_PATH = os.path.join(os.path.dirname(__file__), 'input-json-schema.json')
 
 
 class EnvironmentVariableNames:
@@ -79,6 +60,28 @@ def get_route_wrapper(access_token: Optional[str]) -> RouteWrapper:
     return AccessControlRouteWrapper(access_token)
 
 
+def get_recommendation_html(person_ids: list) -> str:
+    if not person_ids:
+        return '<b>No</b> recommendation available'
+    return '<b>The recommended editors are:</b> %s' % person_ids
+
+
+def get_recommendation_json(person_ids: list) -> dict:
+    return {
+       'person_ids': person_ids,
+       'recommendation_html': get_recommendation_html(person_ids)
+    }
+
+
+def get_response_json(
+        senior_editor_person_ids: list,
+        reviewing_editor_person_ids: list) -> dict:
+    return {
+        'senior_editor_recommendation': get_recommendation_json(senior_editor_person_ids),
+        'reviewing_editor_recommendation': get_recommendation_json(reviewing_editor_person_ids)
+    }
+
+
 def create_app():
     route_wrapper = get_route_wrapper(
         get_peerscout_api_access_token()
@@ -86,24 +89,42 @@ def create_app():
 
     app = Flask(__name__)
 
+    with open(REQUEST_JSON_SCHEMA_PATH) as f:
+        json_schema = json.load(f)
+
+    @app.route('/', methods=['GET'])
+    def _home():
+        html = """<h1>PeerScout Recommendation API</h1>
+        <p>This site is a prototype PeerScout Recommendation API for
+        senior and reviewing editor recommendations.</p>"""
+        return html
+
     @app.route('/api/status', methods=['GET'])
     def _status():
-        return jsonify({'message': 'Welcome to PeerScout'})
+        return jsonify({"status": "OK"})
 
     @app.route('/api/peerscout', methods=['POST'])
     @route_wrapper
     def _peerscout_api():
         data = request.get_json(force=True)
+        LOGGER.info('Processing the request: %s', data)
+
         try:
-            abstract = data['abstract']
-        except KeyError as e:
+            jsonschema.validate(data, json_schema)
+        except jsonschema.exceptions.ValidationError as e:
+            LOGGER.info('invalid JSON %s, %s', data, e)
             raise BadRequest() from e
-        if abstract == 'output1':
-            return jsonify(output1)
-        elif abstract == 'output2':
-            return jsonify(output2)
-        else:
-            raise BadRequest('no valid abstract provided')
+
+        abstract = data['abstract']
+        author_suggestion = data['author_suggestion']
+        suggested_reviewing_editor_person_ids = author_suggestion['include_reviewing_editors_id']
+        suggested_senior_editor_person_ids = author_suggestion['include_senior_editors_id']
+
+        return jsonify(get_response_json(
+            senior_editor_person_ids=suggested_senior_editor_person_ids if abstract else [],
+            reviewing_editor_person_ids=suggested_reviewing_editor_person_ids if abstract else []
+        ))
+
     return app
 
 
