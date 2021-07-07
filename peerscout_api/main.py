d134444dafbb37cd152d7f36c5da3ce92035a4cd
HazalCiplak <hazalciplak@gmail.com>
2021-07-07 11:53:06 +0100

    added query to a .sql file

--- peerscout_api/main.py
+++ peerscout_api/main.py
@@ -49,48 +49,7 @@ NO_RECOMMENDATION_HTML = RECOMMENDATION_HEADINGS[0] + NOT_PROVIDED + \
     RECOMMENDATION_HEADINGS[1] + NOT_PROVIDED + \
     RECOMMENDATION_HEADINGS[2] + NO_RECOMMENDATION_TEXT
 
-QUERY = """
-    SELECT
-        person.Name AS person_name,
-        person.institution,
-        person.Primary_Address.Country AS country,
-        profile.Website_URL AS website,
-        profile.PubMed_URL AS pubmed,
-        profile.Current_Availability AS availability,
-        event.*
-    FROM `{project}.{dataset}.mv_Editorial_Person` AS person
-    INNER JOIN `{project}.{dataset}.mv_Editorial_Editor_Profile` AS profile
-    ON person.person_id = profile.Person_ID
-    LEFT JOIN
-    (SELECT DISTINCT
-        Person.Person_ID AS person_id,
-        CAST(ROUND(PERCENTILE_CONT(
-            Initial_Submission.Reviewing_Editor.Consultation.Days_To_Respond, 0.5
-            ) OVER (PARTITION BY Person.Person_ID),1) AS STRING) AS days_to_respond,
-        CAST(COUNT(DISTINCT Initial_Submission.Reviewing_Editor.Consultation.Request_Version_ID
-            ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS requests,
-        CAST(COUNT(DISTINCT Initial_Submission.Reviewing_Editor.Consultation.Response_Version_ID
-            ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS responses,
-        CAST(ROUND(AVG(Initial_Submission.Reviewing_Editor.Consultation.Has_Response_Ratio
-            ) OVER (PARTITION BY Person.Person_ID)*100,1) AS STRING) AS response_rate,
-        CAST(MAX(Full_Submission.Reviewing_Editor.Current_Assignment_Count
-            ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS no_of_assigments,
-        CAST(COUNT(DISTINCT Full_Submission.Reviewing_Editor.Assigned_Version_ID
-            ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS no_of_full_submissions,
-        CAST(PERCENTILE_CONT(
-            Full_Submission.Reviewing_Editor.Submission_Received_To_Decision_Complete, 0.5
-            ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS decision_time,
-        FROM
-        `{project}.{dataset}.mv_Editorial_Editor_Workload_Event`,
-        UNNEST(Person.Roles) AS person_role
-        WHERE DATE(Event_Timestamp)
-            BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 12 MONTH) AND CURRENT_DATE()
-        AND person_role.Role_Name='Editorial Board Member'
-        AND Person.Person_ID IN UNNEST({person_ids})
-    ) AS event
-    ON person.person_id = event.person_id
-    WHERE person.person_id IN UNNEST({person_ids})
-"""
+QUERY_PATH = os.path.join(os.path.dirname(__file__), 'person_details_and_stats.sql')
 
 LOGGER = logging.getLogger(__name__)
 
@@ -132,8 +91,11 @@ def query_bq_for_person_details(
 
     client = Client(project=project)
 
+    with open(QUERY_PATH) as f:
+        sql_file = f.read()
+
     sql = (
-        QUERY.format(
+        sql_file.format(
             project=project,
             dataset=dataset,
             person_ids=person_ids
---
+++ peerscout_api/person_details_and_stats.sql
@@ -0,0 +1,39 @@
+SELECT
+    person.Name AS person_name,
+    person.institution,
+    person.Primary_Address.Country AS country,
+    profile.Website_URL AS website,
+    profile.PubMed_URL AS pubmed,
+    profile.Current_Availability AS availability,
+    event.*
+FROM `{project}.{dataset}.mv_Editorial_Person` AS person
+INNER JOIN `{project}.{dataset}.mv_Editorial_Editor_Profile` AS profile
+ON person.person_id = profile.Person_ID
+LEFT JOIN
+(SELECT DISTINCT
+    Person.Person_ID AS person_id,
+    CAST(ROUND(PERCENTILE_CONT(
+        Initial_Submission.Reviewing_Editor.Consultation.Days_To_Respond, 0.5
+        ) OVER (PARTITION BY Person.Person_ID),1) AS STRING) AS days_to_respond,
+    CAST(COUNT(DISTINCT Initial_Submission.Reviewing_Editor.Consultation.Request_Version_ID
+        ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS requests,
+    CAST(COUNT(DISTINCT Initial_Submission.Reviewing_Editor.Consultation.Response_Version_ID
+        ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS responses,
+    CAST(ROUND(AVG(Initial_Submission.Reviewing_Editor.Consultation.Has_Response_Ratio
+        ) OVER (PARTITION BY Person.Person_ID)*100,1) AS STRING) AS response_rate,
+    CAST(MAX(Full_Submission.Reviewing_Editor.Current_Assignment_Count
+        ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS no_of_assigments,
+    CAST(COUNT(DISTINCT Full_Submission.Reviewing_Editor.Assigned_Version_ID
+        ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS no_of_full_submissions,
+    CAST(PERCENTILE_CONT(
+        Full_Submission.Reviewing_Editor.Submission_Received_To_Decision_Complete, 0.5
+        ) OVER (PARTITION BY Person.Person_ID) AS STRING) AS decision_time,
+    FROM `{project}.{dataset}.mv_Editorial_Editor_Workload_Event`,
+    UNNEST(Person.Roles) AS person_role
+    WHERE DATE(Event_Timestamp)
+        BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 12 MONTH) AND CURRENT_DATE()
+    AND person_role.Role_Name='Editorial Board Member'
+    AND Person.Person_ID IN UNNEST({person_ids})
+) AS event
+ON person.person_id = event.person_id
+WHERE person.person_id IN UNNEST({person_ids})
