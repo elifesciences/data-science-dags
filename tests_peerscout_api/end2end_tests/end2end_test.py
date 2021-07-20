65e4834a4563b078177e7a05861711bc71daf6df
HazalCiplak <hazalciplak@gmail.com>
2021-07-20 09:52:12 +0100

    added url to a global variable

--- tests_peerscout_api/end2end_tests/end2end_test.py
+++ tests_peerscout_api/end2end_tests/end2end_test.py
@@ -3,16 +3,15 @@ import requests
 
 LOGGER = logging.getLogger(__name__)
 
+# to run the test locally use the url below:
+# URL = 'http://localhost:8090/api/peerscout'
+URL = 'http://peerscout-api:8080/api/peerscout'
+
 
 def test_get_response_json():
     with open('./peerscout_api/example-data/peerscout-api-request1.json') as request_json:
-        # to run the test locally use url below:
-        # url = 'http://localhost:8090/api/peerscout'
-
-        url = 'http://peerscout-api:8080/api/peerscout'
         headers = {'Content-Type': 'application/json'}
-
-        resp = requests.post(url, data=request_json, headers=headers)
+        resp = requests.post(URL, data=request_json, headers=headers)
         LOGGER.info('request: %s', resp.request)
         assert resp.status_code == 200
         LOGGER.info('reponse: %s', resp)
