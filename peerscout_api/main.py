f2c79007c44de3b83908239056da9ef087b07978
HazalCiplak <hazalciplak@gmail.com>
2021-12-02 12:25:28 +0000

    added double quote and html.escape funtion between url

--- peerscout_api/main.py
+++ peerscout_api/main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+import html
 from typing import NamedTuple, Optional
 import jsonschema
 
@@ -157,9 +158,11 @@ def get_html_text_for_recommended_person(
         + (('<br /><span style=\'color:red;\'><strong>!</strong></span> Limited availability: '
             + person.availability) if person.availability else '')
         + ('<br />' if (person.website or person.pubmed) else '')
-        + (('<a href=' + person.website + ' target="_blank">Website</a>') if person.website else '')
+        + (('<a href="' + html.escape(person.website) + '" target=_blank>Website</a>')
+            if person.website else '')
         + (' | ' if (person.website and person.pubmed) else '')
-        + (('<a href=' + person.pubmed + ' target="_blank">PubMed</a>') if person.pubmed else '')
+        + (('<a href="' + html.escape(person.pubmed) + '" target=_blank>PubMed</a>')
+            if person.pubmed else '')
         + ('<br />' if (
             person.days_to_respond
             or person.requests
@@ -366,10 +369,10 @@ def create_app():
 
     @app.route('/', methods=['GET'])
     def _home():
-        html = """<h1>PeerScout Recommendation API</h1>
+        html_result = """<h1>PeerScout Recommendation API</h1>
         <p>PeerScout Recommendation API for
         senior and reviewing editor recommendations.</p>"""
-        return html
+        return html_result
 
     @app.route('/api/status', methods=['GET'])
     def _status():
