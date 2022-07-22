1cf3c95116e33fa92978718ef3a77b4aa6a33bb8
HazalCiplak <hazalciplak@gmail.com>
2022-07-22 09:25:30 +0300

    codereview changes

--- data_science_pipeline/utils/editor_recommendation.py
+++ data_science_pipeline/utils/editor_recommendation.py
@@ -2,11 +2,11 @@ from typing import Iterable
 import pandas as pd
 
 
-def get_author_ids_of_given_version_of_paper(
-    manuscript_df: pd.DataFrame,
+def get_author_ids_of_given_version_of_manuscript(
+    manuscript_version_df: pd.DataFrame,
     version_id: str
 ) -> Iterable[str]:
-    prediction_manuscript_version_df = manuscript_df.loc[
-        manuscript_df['version_id'] == version_id
+    prediction_manuscript_version_df = manuscript_version_df.loc[
+        manuscript_version_df['version_id'] == version_id
     ]
     return prediction_manuscript_version_df['author_person_ids'].values[0]
--- tests/utils/editor_recommendation_test.py
+++ tests/utils/editor_recommendation_test.py
@@ -1,47 +1,38 @@
 import pandas as pd
 
 from data_science_pipeline.utils.editor_recommendation import (
-    get_author_ids_of_given_version_of_paper
+    get_author_ids_of_given_version_of_manuscript
 )
 
-MANUSCRIPT_VERSION_1 = "match_version_id_1"
-
+MANUSCRIPT_VERSION_ID_1 = "manuscript_version_id_1"
 AUTHOR_ID_LIST_1 = [
-    "match_author_id1",
-    "match_author_id2"
+    "manuscript_version_id_1_author_id1",
+    "manuscript_version_id_1_author_id2"
 ]
 
-MANUSCRIPT_VERSION_2 = "unmatch_version_id_1"
+MANUSCRIPT_VERSION_ID_2 = "manuscript__version_id_2"
+AUTHOR_ID_LIST_2 = ["manuscript__version_id_2_author_id1"]
 
-AUTHOR_ID_LIST_2 = [
-    "unmatch_author_id1"
-]
-
-MANUSCRIPT_DICT = {
-  "version_id": {
-    "0": MANUSCRIPT_VERSION_1,
-    "1": MANUSCRIPT_VERSION_2
-  },
-  "author_person_ids": {
-    "0": AUTHOR_ID_LIST_1,
-    "1": AUTHOR_ID_LIST_2
-  }
-}
-
-MANUSCRIPT_DF = pd.DataFrame(MANUSCRIPT_DICT)
-
-print(MANUSCRIPT_DF)
+MANUSCRIPT_VERSION_DF = pd.DataFrame(
+    [{
+        'version_id': MANUSCRIPT_VERSION_ID_1,
+        'author_person_ids': AUTHOR_ID_LIST_1
+    }, {
+        'version_id': MANUSCRIPT_VERSION_ID_2,
+        'author_person_ids': AUTHOR_ID_LIST_2
+    }]
+)
 
 
 class TestGetAuthorIdsOfGivenVersionOfPaper:
-    def test_should_return_author_ids_for_matching_manuscript_version(self):
-        assert get_author_ids_of_given_version_of_paper(
-            MANUSCRIPT_DF,
-            MANUSCRIPT_VERSION_1
+    def test_should_return_matching_author_ids_for_given_manuscript_version_id(self):
+        assert get_author_ids_of_given_version_of_manuscript(
+            MANUSCRIPT_VERSION_DF,
+            MANUSCRIPT_VERSION_ID_1
         ) == AUTHOR_ID_LIST_1
 
-    def test_should_not_return_author_ids_for_unmatching_manuscript_version(self):
-        assert get_author_ids_of_given_version_of_paper(
-            MANUSCRIPT_DF,
-            MANUSCRIPT_VERSION_2
-        ) != AUTHOR_ID_LIST_1
+    def test_should_return_matching_author_ids_for_another_given_manuscript_version_id(self):
+        assert get_author_ids_of_given_version_of_manuscript(
+            MANUSCRIPT_VERSION_DF,
+            MANUSCRIPT_VERSION_ID_2
+        ) == AUTHOR_ID_LIST_2
