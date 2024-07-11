f70d4f48c602871ada2d63a9c27dcb63e65b3235
Hazal <HazalCiplak@users.noreply.github.com>
2024-07-11 10:03:02 +0100

    Used prophet instead of fbprophet (#633)
    * used prophet instead of fbprophet
    
    * updated file name to requirements.prophet.txt
    
    * created necessary jovyan paths

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pip install --user -r $DIR/requirements.dag.txt
 pip install --user -r $DIR/requirements.notebook.txt
-pip install --user -r $DIR/requirements.fbprophet.txt
+pip install --user -r $DIR/requirements.prophet.txt
 pip install -e $DIR/ --user
 cp $DIR/dags $1 -r
 cp $DIR/notebooks $2/ -r
