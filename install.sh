618890d66f76f5873dd97f01fe391b415fbcb776
HazalCiplak <hazalciplak@gmail.com>
2024-10-31 18:44:30 +0000

    removed dags folder related processes

--- install.sh
+++ install.sh
@@ -7,5 +7,4 @@ pip install --user -r $DIR/requirements.dag.txt
 pip install --user -r $DIR/requirements.notebook.txt
 pip install --user -r $DIR/requirements.prophet.txt
 pip install -e $DIR/ --user
-cp $DIR/dags $1 -r
 cp $DIR/notebooks $2/ -r
