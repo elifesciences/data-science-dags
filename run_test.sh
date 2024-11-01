800575181d0323da0bc376c805f7850f2158f946
HazalCiplak <hazalciplak@gmail.com>
2024-11-01 12:24:04 +0000

    removed `AIRFLOW__CORE__FERNET_KEY` command from run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
-export AIRFLOW__CORE__FERNET_KEY
-
 # avoid issues with .pyc/pyo files when mounting source directory
 export PYTHONOPTIMIZE=
 
