eda011b5a773af1c6ecaecf170cf4638497b4e44
Daniel Ecer <de-code@users.noreply.github.com>
2025-01-06 16:57:47 +0000

    Call api without post body

--- peerscout_api/spacy_api_keyword_extractor.py
+++ peerscout_api/spacy_api_keyword_extractor.py
@@ -1,6 +1,26 @@
 from dataclasses import dataclass
+from typing import Iterable, List
+
+import requests
+
+from elife_data_hub_utils.keyword_extract.extract_keywords import (
+    KeywordExtractor
+)
+
+
+DEFAULT_TIMEOUT = 60
 
 
 @dataclass(frozen=True)
-class SpaCyApiKeywordExtractor():
+class SpaCyApiKeywordExtractor(KeywordExtractor):
     api_url: str
+
+    def iter_extract_keywords(
+        self,
+        text_list: Iterable[str]
+    ) -> Iterable[List[str]]:
+        requests.post(
+            url=self.api_url,
+            timeout=DEFAULT_TIMEOUT
+        )
+        return []
---
+++ tests_peerscout_api/unit_tests/spacy_api_keyword_extractor_test.py
@@ -0,0 +1,32 @@
+
+
+from typing import Iterable
+from unittest.mock import ANY, MagicMock, patch
+import pytest
+
+import peerscout_api.spacy_api_keyword_extractor as target_module
+from peerscout_api.spacy_api_keyword_extractor import SpaCyApiKeywordExtractor
+
+
+TEST_SPACY_API_URL_1 = 'http://example/spacy-url-1'
+
+
+@pytest.fixture(name='requests_mock', autouse=True)
+def _requests_mock() -> Iterable[MagicMock]:
+    with patch.object(target_module, 'requests') as mock:
+        yield mock
+
+
+@pytest.fixture(name='requests_post_mock', autouse=True)
+def _requests_post_mock(requests_mock: MagicMock) -> MagicMock:
+    return requests_mock.post
+
+
+class TestSpaCyApiKeywordExtractor:
+    def test_should_call_api(self, requests_post_mock: MagicMock):
+        keyword_extractor = SpaCyApiKeywordExtractor(api_url=TEST_SPACY_API_URL_1)
+        keyword_extractor.iter_extract_keywords(text_list=['text1'])
+        requests_post_mock.assert_called_with(
+            url=TEST_SPACY_API_URL_1,
+            timeout=ANY
+        )
