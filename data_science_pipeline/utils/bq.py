a9329478386ed32f952e8df890468926ade5e0b5
HazalCiplak <hazalciplak@gmail.com>
2025-05-16 09:53:52 +0100

    Disable pylint warning for too many positional arguments in multiple functions

--- data_science_pipeline/utils/bq.py
+++ data_science_pipeline/utils/bq.py
@@ -85,6 +85,7 @@ def get_validated_dataset_name_table_name(
     return dataset_name, table_name
 
 
+# pylint: disable=too-many-positional-arguments
 def load_file_into_bq(
         filename: Path,
         project_id: str,
